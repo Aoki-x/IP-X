62f1f4c0e09a4fb0893917747d189e12f271d1e3
Aoki-x <72118930+Aoki-x@users.noreply.github.com>
2023-01-30 08:49:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import requests
 import os
 from termcolor import  colored
+from time import sleep
+
+#A função abaixo consulta o pais do usuário para traduzir os nomes(paises, continentes, etc) de acordo com o seu pais. 
+def my_country(my_ip):
+    req_country = requests.get(f"https://ipwho.is/{my_ip}")
+    data = req_country.json()
+    if data['country'] == 'Brazil':
+        return "pt-BR"
+    else:
+        return "en"
+
+def text_exit():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(colored("\t\tVISITE O MEU PERFIL DO GITHUB: Aoki-x", "yellow"))
+    sleep(2)
 
 while True:
+
         
     os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -24,49 +40,43 @@ while True:
 
     ''', "red"))
 
-    #A função abaixo consulta o pais do usuário para traduzir os nomes(paises, continentes, etc) de acordo com o seu pais. 
-    def my_country(my_ip):
-        req_country = requests.get(f"https://ipwho.is/{my_ip}")
-        data = req_country.json()
-        if data['country'] == 'Brazil':
-            return "pt-BR"
-        else:
-            return "en"
-
-
     try:
     #Aqui o sistema tentará pegar o seu ip externo.
-        req_myip = requests.get("http://checkip.dyndns.org")
-        my_ip = req_myip.text.split(" ")[-1].strip().split("<")[0].strip()
-        print(f"\t(SEU IP: {my_ip}) | {my_country(my_ip)}")
+        req_myip = requests.get("https://ipwho.is/")
+        my_ip = req_myip.json()
+        ip = my_ip['ip']
+        print(f"\t(SEU IP: {ip}) | {my_country(ip)}")
     except:
-        print(" Houve um erro ao tentar exibir o seu ip.")
+        print(colored("\tHouve um erro ao tentar exibir o seu ip.", "red"))
 
     #Aqui daremos início a consulta a API do Whois
-    ip = input("\n\tDigite o ip do alvo ou aperte ENTER-> ")
-    req_api = requests.get(f"https://ipwho.is/{ip}?lang={my_country(my_ip)}")
+    ip = input("\n\tDigite o ip do alvo ou N para sair: ")
+    if ip == 'N' or ip == 'n':
+        text_exit()
+        break
+    else:
+        req_api = requests.get(f"https://ipwho.is/{ip}?lang={my_country(ip)}")
 
     #Aqui verificaremos o status da API, se for 200 é porque não houve problemas.
     if req_api.status_code == 200:
 
-    #Aqui na variável data puxaremos o Json. Caso queira ver, logo abaixo da variável coloque - > print(data)
         data = req_api.json()
         print(f'''
-            SUCCESS: {data['success']}
+            SUCCESS: {colored(data['success'], "blue")}
             \n\t  -----------------\n
-            IP: {data['ip']}\n
+            IP: {colored(data['ip'], "yellow")}\n
             TIPO: {data['type']}\n
             CONTINENTE: {data['continent']}| {data['continent_code']}\n
-            PAIS: {data['country']}| {data['country_code']}\n
-            CAPITAL: {data['capital']}\n
-            REGIÃO: {data['region']}| {data['region_code']}\n
-            CIDADE: {data['city']}\n
+            PAIS: {colored(data['country'], "green")}| {data['country_code']}\n
+            CAPITAL: {colored(data['capital'], "green")}\n
+            REGIÃO: {colored(data['region'], "green")}| {data['region_code']}\n
+            CIDADE: {colored(data['city'], "green")}\n
             LATITUDE: {data['latitude']}\n
             LONGITUDE: {data['longitude']}\n
             CONEXÕES: \n
-            \t |----->ORG: {data['connection']['org']}\n
-            \t |----->FORNECEDOR: {data['connection']['isp']}\n
-            \t |----->DOMÍNIO: {data['connection']['domain']}\n
+            \t |----->ORG: {colored(data['connection']['org'], "blue")}\n
+            \t |----->FORNECEDOR: {colored(data['connection']['isp'], "blue")}\n
+            \t |----->DOMÍNIO: {colored(data['connection']['domain'], "blue")}\n
             FUSO HORARIO: \n
             \t |----->ID: {data['timezone']['id']}\n
             \t |----->ABBR: {data['timezone']['abbr']}\n
@@ -85,11 +95,14 @@ while True:
             ''')
         except:
             print(colored("\tNão foi possível consultar a segurança do alvo.", "red"))
+        
+        sleep(1)
 
-        back = input(f"\tDeseja consultar novamente?\n\t1 - Sim(Yes)\n\t2 - Não(No)\n\t|-->")
+        back = input(f"\n\tDeseja consultar novamente?\n\t1 - Sim(Yes)\n\t2 - Não(No)\n\t|-->")
         if back == '1':
             continue
         else:
+            text_exit()
             break
     else:
-        print("Ocorreu um erro, verifique a sua Internet.")
+        print("\t\tOcorreu um erro, verifique a sua Internet.")
